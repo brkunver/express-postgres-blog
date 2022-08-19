2f06e2b3edc76a904623a6e3b7b00892aa201870
burakhaunver <burakhanunver@gmail.com>
2022-08-19 12:20:40 +0300

    database url change

--- database.js
+++ database.js
@@ -1,15 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 //init database
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
 
