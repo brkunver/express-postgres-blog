13e8c95309b16632bd61b45311a2a82e5e8382c6
burakhaunver <burakhanunver@gmail.com>
2022-08-19 12:31:19 +0300

    fix database connection

--- database.js
+++ database.js
@@ -1,7 +1,19 @@
 const { Sequelize } = require("sequelize");
 
 //init database
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-
+const sequelize = new Sequelize({
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  },
+});
 module.exports = sequelize;
 
